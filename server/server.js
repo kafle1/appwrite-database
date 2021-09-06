a54b0c2575ca8ab98fe161e00e2d67307e716af5
Niraj Kafle <62476876+kafle1@users.noreply.github.com>
2021-09-06 19:28:15 +0545

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,9 +24,9 @@ export const database = new sdk.Database(client);
 
 client
   .setEndpoint("http://localhost/v1") // Your API Endpoint
-  .setProject("61308ccc181d0") // Your project ID
+  .setProject("") // Your project ID
   .setKey(
-    "524168baf8489844dec669b0895f6b6bfcabc5553868c2c2fae66184f32bb7b5c67964245c3497c45827b11c7c092323cfe0af8846d749ab4166d341f2a0ceb5c471edb0d130f8157f243d7c45e671c940eca443d2510a3c36c450b870e5e80f0c85c6caf6231fe96c6a9f510764fb4207abbda8bdbf5c341f5ced4602ff8415"
+    ""
   ); // Your secret API key
 
 // LISTENING TO PORT
